ae6342e80a71801471468523b3800b7fc3f121b8
uselessthings-tests <98508894+uselessthings-tests@users.noreply.github.com>
2022-01-28 10:29:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ option = st.selectbox(
 if st.button('Convert'):
     if option == 'MP4':
         video_parse = YouTube(textbox)
-        video_with_audio_hd  = str(video_parse.vid_info).split("'itag': 22,")[1].split(", 'mimeType':")[0].removeprefix(" 'url': '").removesuffix("'")
+        video_with_audio_hd  = str(video_parse.vid_info).split("'itag': 22,")[1].split(", 'mimeType':")[0].replace("'url': '"," ").replace("'","").replace(" ","")
         st.write("Your video is ready to be downloaded \n visit this link to download the video 👇")
         st.write(shorten.tinyurl.short(video_with_audio_hd))
+
     if option == 'MP3':
         video_parse = YouTube(textbox)
         video_audio = video_parse.streams.filter(only_audio=True).first().url
